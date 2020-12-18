d5ba72340d73147a6242980fdbd7e7cf5b1725b0
hamishmack1 <hamishmack01@gmail.com>
2020-12-18 12:48:27 +1000

    Cleaned up code. Reduced length.

--- highlights.py
+++ highlights.py
@@ -21,7 +21,6 @@ def find_location(info):
 def read_clippings():
 	count = 0
 	raw_highlight_info = []
-	highlight_num = 0
 	all_highlights = []
 
 	with open('clippings', 'rt') as myfile:
@@ -33,56 +32,43 @@ def read_clippings():
 					raw_highlight_info.append(line.rstrip('\n'))
 					count += 1
 					if count == 3:
-						highlight_num += 1
 						all_highlights.append(raw_highlight_info)
 						count = 0
 						raw_highlight_info = []
-		return highlight_num, all_highlights
+		return all_highlights
 
-def remove_dups(highlight_num, all_highlights):
+def create_library(all_highlights):
 	i = 0
-	while i < highlight_num - 1:
+	library = []
+	found = False
+	while i < len(all_highlights) - 1:
 		if abs(find_location(all_highlights[i][1]) - find_location(all_highlights[i + 1][1])) < 10:
 			all_highlights.pop(i)
-			highlight_num -= 1
 		else:
+			for book in library:
+				if all_highlights[i][0] == book.get_title_auth():
+					book.add_highlight(all_highlights[i][2])
+					found = True
+			if not found:
+				library.append(Book(all_highlights[i][0], []))
+				library[len(library) - 1].add_highlight(all_highlights[i][2])
+			found = False
 			i += 1
-	return highlight_num
-
-def create_library(highlight_num, all_highlights):
-	library = []
-	i = 0
-	found = 0
-	while i < highlight_num:
-		for j in library:
-			if all_highlights[i][0] == j.get_title_auth():
-				j.add_highlight(all_highlights[i][2])
-				found = 1
-		if found == 0:
-			library.append(Book(all_highlights[i][0], []))
-			library[len(library) - 1].add_highlight(all_highlights[i][2])
-
-		found = 0
-		i += 1
-
 	return library
 
 def main():
-	highlight_num, all_highlights = read_clippings()
-	new_highlight_num = remove_dups(highlight_num, all_highlights)
-	library = create_library(new_highlight_num, all_highlights)
+	library = create_library(read_clippings())
 
 	print("Welcome To Your Highlight Library!")
 
 	for book in library:
 		print('[' + str(library.index(book) + 1) + '] ' + book.get_title_auth()) 
 
-	book_index = int(input("What Book Would You Like To View? "))
+	book_index = int(input("What Book Would You Like To View? ")) - 1
 	print("")
 
-	print(library[book_index - 1].get_title_auth())
-	print("----------\n")
-	for highlight in library[book_index - 1].get_highlights():
+	print(library[book_index].get_title_auth() + '\n' +  "----------\n")
+	for highlight in library[book_index].get_highlights():
 		print(highlight, '\n')
 
 	menu = input("Would You Like To Return To Main Menu? (Y/N) ")
